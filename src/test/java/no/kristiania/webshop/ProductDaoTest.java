468fbcc010f01fc9a548d1c392df2c2371ae135a
Magnus <linmag18@student.kritstiania.no>
2019-10-30 15:32:53 +0100

    Extract AbstractDao from ProductDao

---
+++ src/test/java/no/kristiania/webshop/ProductDaoTest.java
@@ -0,0 +1,38 @@
+package no.kristiania.webshop;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ProductDaoTest {
+
+    @Test
+    void shouldListInsertedProduct()throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl ("jdbc:h2:mem:myTestDatabase");
+
+
+        dataSource.getConnection ().createStatement(). executeUpdate(
+                "create table PRODUCTS(id serial primary key, name varchar (1000) not null)"
+
+        );
+
+
+        ProductDao dao = new ProductDao(dataSource);
+        String product = sampleProduct ();
+        dao.insert(product);
+        assertThat(dao.listAll ())
+                .contains (product);
+    }
+
+
+    private  String sampleProduct() {
+        return null;
+
+    }
+
+
+}
